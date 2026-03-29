23da63532ca743e27d63421260a7d0b09fea58f1
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2026-03-29 14:31:23 +0200

    Blog: card visible (bordure + ombre) + sommaire ancré automatique depuis les H2

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -13,7 +13,7 @@ class BlogController < ApplicationController
     render "errors/not_found", status: :not_found and return unless @article
     @page_title  = @article[:title]
     @page_desc   = @article[:meta_description]
-    @article_html = render_markdown(@article[:body])
+    @article_html, @toc = render_markdown_with_toc(@article[:body])
     expires_in 1.hour, public: true
   end
 
@@ -48,9 +48,18 @@ class BlogController < ApplicationController
     result
   end
 
-  def render_markdown(text)
+  def render_markdown_with_toc(text)
     renderer = Redcarpet::Render::HTML.new(hard_wrap: false, safe_links_only: true)
     md = Redcarpet::Markdown.new(renderer, tables: true, no_intra_emphasis: true, autolink: false)
-    md.render(text).html_safe
+    html = md.render(text)
+    doc = Nokogiri::HTML::DocumentFragment.parse(html)
+    toc = []
+    doc.css('h2').each do |h2|
+      heading_text = h2.text.strip
+      anchor = heading_text.parameterize
+      h2['id'] = anchor
+      toc << { text: heading_text, anchor: anchor }
+    end
+    [doc.to_html.html_safe, toc]
   end
 end
